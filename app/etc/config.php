d7a164d93d08f2401895b9e44f1052f082dafbac
Jeff Britts <jbritts@adobe.com>
2024-04-18 14:45:58 -0500

    updates

--- app/etc/config.php
+++ app/etc/config.php
@@ -580,6 +580,8 @@ return [
         'Amasty_Rules' => 1,
         'Amasty_RulesPro' => 1,
         'Amasty_SalesRuleWizard' => 1,
+        'MageMojo_Cron' => 1,
+        'MagentoEse_AdminConfigurations' => 1,
         'MagentoEse_AutoFill' => 1,
         'MagentoEse_DataInstall' => 1,
         'MagentoEse_DataInstallGraphQl' => 1,
